d70bbc29f99dedd31424527520af35962845b2cd
canikwe <c.anikwe@gmail.com>
2019-03-09 14:33:10 -0500

    remove whitespace

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -6,25 +6,13 @@ export default class ColorBox extends Component {
     const newValue = this.props.opacity - 0.1;
     
     if (this.props.opacity >= 0.2) {
-      
-//      let hello = this.render
-      
       return (
         <div className="color-box" style={{opacity: this.props.opacity}}>
           <ColorBox opacity={newValue} />
-        </div>
-        
+        </div> 
       )
-
     } else if (this.props.opacity < 0.2){
       return null
     }
-    
-    
-    
-    
-    
-
   }
-  
 }
